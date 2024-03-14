23c2ca4bb9a81de24dd724e433bfa308e26bf023
eslam <eslam@localhost.localdomain>
2024-03-15 00:26:40 +0200

    check requested_fields if None

--- CypartaGraphqlSubscriptionsTools/utils.py
+++ CypartaGraphqlSubscriptionsTools/utils.py
@@ -6,11 +6,16 @@ def get_model_name_instance(ModelType):
 
 
 def filter_requested_fields(deserialized_data,requested_fields):
-     
+ 
+            
+    if requested_fields is not None:
         exposed_fields = {
             field: deserialized_data['fields'][field]
             for field in requested_fields
             if field in deserialized_data['fields']
         }
-        deserialized_data['fields']=exposed_fields
-        return deserialized_data
+    else:
+        # If requested_fields is None, expose all fields or handle as needed
+        exposed_fields = deserialized_data['fields']
+    deserialized_data['fields']=exposed_fields
+    return deserialized_data
